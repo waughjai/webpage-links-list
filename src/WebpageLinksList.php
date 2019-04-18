70b4f43692b6a8fa80d858ab0c25c14aeb8423c5
Jaimeson Waugh <jaimeson@4cesi.com>
2019-04-18 10:59:30 -0700

    Create generally functioning behavior

--- src/WebpageLinksList.php
+++ src/WebpageLinksList.php
@@ -10,17 +10,14 @@ namespace WaughJ\WebpageLinksList
 
 	class WebpageLinksList
 	{
-		public function __construct( string $url )
+		public function __construct( string $url, int $limit = -1 )
 		{
 			$this->tried_list = [];
-			$this->loops = 0;
 			$this->list = [];
+			$this->list_count = 0;
+			$this->limit = $limit;
 			$uri = new URI( $url );
 			$this->generateListFromURL( $url, $uri->getHost() );
-			foreach ( $this->list as $item )
-			{
-				//echo "{$item}\n";
-			}
 		}
 
 		public function getList() : array
@@ -28,54 +25,75 @@ namespace WaughJ\WebpageLinksList
 			return $this->list;
 		}
 
-		private function generateListFromURL( string $url, string $host )
+		private function tryURL( string $url, string $host ) : void
 		{
-			if ( count( $this->list ) < 100 && !in_array( $url, $this->tried_list ) )
+			$url = $this->removeFragmentFromURL( $url );
+			if ( $this->testHasReachedLimit() && !in_array( $url, $this->tried_list ) )
 			{
-				echo "$url\n";
-				try
-				{
-					$response = Request::get( $url );
-					$data = new Crawler( $response->body );
-					$this->tried_list[] = $url;
-					$this->list[] = $url;
-					$this->loops++;
-					$data->filter( 'a' )->each
-					(
-						function( Crawler $node, $i )
+				$this->tried_list[] = $url;
+				$this->list[] = $url;
+				$this->list_count++;
+				$this->generateListFromURL( $url, $host );
+			}
+		}
+
+		private function generateListFromURL( string $url, string $host ) : void
+		{
+			try
+			{
+				$response = Request::get( $url );
+				$data = new Crawler( $response->body );
+				$data->filter( 'a' )->each
+				(
+					function( Crawler $node, $i ) use ( $host )
+					{
+						if ( is_string( $node->attr( 'href' ) ) )
 						{
-							if ( is_string( $node->attr( 'href' ) ) )
+							$link_uri = new URI( $node->attr( 'href' ) );
+							if ( $link_uri->getHost() && $this->testHostsMatch( $host, $link_uri->getHost() ) )
 							{
-								$link_uri = new URI( $node->attr( 'href' ) );
-								if ( $host === $link_uri->getHost() )
+								if ( $link_uri->isRelative() )
 								{
-									if ( $link_uri->isRelative() )
-									{
-										$parent = new Directory( $url );
-										$link_url = new Directory([ $parent->getString([ 'ending-slash' => true, 'starting-slash' => false ]), $node->attr( 'href' ) ]);
-										$link_url = $link_url->getString([ 'ending-slash' => false, 'starting-slash' => false ]);
-									}
-									else
-									{
-										$link_url = $node->attr( 'href' );
-									}
-									$this->generateListFromURL( $link_url, $host );
+									$parent = new Directory( $url );
+									$link_url = new Directory([ $parent->getString([ 'ending-slash' => true, 'starting-slash' => false ]), $node->attr( 'href' ) ]);
+									$link_url = $link_url->getString([ 'ending-slash' => false, 'starting-slash' => false ]);
 								}
+								else
+								{
+									$link_url = $node->attr( 'href' );
+								}
+								$this->tryURL( $link_url, $host );
 							}
 						}
-					);
-				}
-				catch ( \Exception $e )
-				{
-				}
+					}
+				);
 			}
-			else
+			catch ( \Exception $e )
 			{
+				// We should just ignore broken links & carry on.
+				return;
 			}
+		}
+
+		private function testHostsMatch( string $host1, string $host2 ) : bool
+		{
+			return $host1 === $host2 || "www.{$host1}" === $host2 || $host1 === "www.{$host2}";
+		}
+
+		private function removeFragmentFromURL( string $url ) : string
+		{
+			$uri = new URI( $url );
+			$uri->setFragment( '' );
+			return $uri->getUri();
+		}
+
+		private function testHasReachedLimit() : bool
+		{
+			return $this->limit === -1 || $this->list_count < $this->limit;
 		}
 
 		private $list;
 		private $tried_list;
-		private $loops;
+		private $list_count;
 	}
 }
--- tests/WebpageLinksListTest.php
+++ tests/WebpageLinksListTest.php
@@ -5,13 +5,8 @@ use Unirest\Request;
 use WaughJ\WebpageLinksList\WebpageLinksList;
 
 class WebpageLinksListTest extends TestCase
-{/*
-	public function testRequestWorks()
-	{
-		$response = Request::get( 'https://www.google.com' );
-		$this->assertContains( '<html itemscope="" itemtype="http://schema.org/WebPage" lang="en">', $response->body );
-	}*/
-
+{
+	/*
 	public function testBrokenPage()
 	{
 		$list = new WebpageLinksList( 'https://www.asfsafd.com' );
@@ -20,7 +15,16 @@ class WebpageLinksListTest extends TestCase
 
 	public function testLinksListRecursive()
 	{
-		$list = new WebpageLinksList( 'https://www.northwestgoldcoast.com/' );
-		$this->assertContains( 'https://www.northwestgoldcoast.com/2009/02/26/best-bites-lives-up-to-its-name/', $list->getList() );
+		$list = new WebpageLinksList( 'https://www.jaimeson-waugh.com/' );
+		$this->assertContains( 'https://jaimeson-waugh.com/mega-microstories/', $list->getList() );
+		$this->assertContains( 'https://jaimeson-waugh.com/boskeopolis-land/', $list->getList() );
+	}
+	*/
+
+	public function testLinksWithoutImages()
+	{
+		$list = new WebpageLinksList( 'https://en.wikipedia.org/wiki/PHP', 100 );
+		var_dump( $list->getList() );
+		$this->assertNotContains( '.svg', $list->getList() );
 	}
 }
